8ffb975f0402fce7f920f7cae9d56d7cd4c93211
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-27 23:00:07 +0300

    Added MinMaxAttribute

---
+++ Assets/Scripts/Utils/Editor/MinMaxDrawer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Utils.Editor
+{
+    [CustomPropertyDrawer(typeof(MinMax))]
+    [CustomPropertyDrawer(typeof(MinMaxAttribute))]
+    public class MinMaxDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (property.serializedObject.isEditingMultipleObjects)
+            {
+                return 0f;
+            }
+            
+            return base.GetPropertyHeight(property, label) + 16f;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.serializedObject.isEditingMultipleObjects)
+            {
+                return;
+            }
+
+            var minProperty = property.FindPropertyRelative("Min");
+            var maxProperty = property.FindPropertyRelative("Max");
+            var minMaxAttribute = attribute as MinMaxAttribute ?? new MinMaxAttribute(0, 1);
+            position.height -= 16f;
+
+            label = EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            var min = minProperty.floatValue;
+            var max = maxProperty.floatValue;
+
+            var left = new Rect(position.x, position.y, position.width / 2 - 11f, position.height);
+            var right = new Rect(position.x + position.width - left.width, position.y, left.width, position.height);
+            var mid = new Rect(left.xMax, position.y, 22, position.height);
+            min = Mathf.Clamp(EditorGUI.FloatField(left, min), minMaxAttribute.Min, max);
+            EditorGUI.LabelField(mid, " to ");
+            max = Mathf.Clamp(EditorGUI.FloatField(right, max), min, minMaxAttribute.Max);
+
+            position.y += 16f;
+            EditorGUI.MinMaxSlider(position, GUIContent.none, ref min, ref max, minMaxAttribute.Min, minMaxAttribute.Max);
+
+            minProperty.floatValue = min;
+            maxProperty.floatValue = max;
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/MinMaxAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public class MinMaxAttribute : PropertyAttribute
+    {
+        public readonly float Min;
+        public readonly float Max;
+
+        public MinMaxAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+    
+    [Serializable] public struct MinMax
+    {
+        [SerializeField] public float Min;
+        [SerializeField] public float Max;
+
+        public float RandomValue => UnityEngine.Random.Range(Min, Max);
+
+        public MinMax(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public float Clamp(float value)
+        {
+            return Mathf.Clamp(value, Min, Max);
+        }
+    }
+}
