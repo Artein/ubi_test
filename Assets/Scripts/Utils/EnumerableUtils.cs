34a0df23ea7656af36f11b79cad96c3881d08a9e
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-27 16:34:43 +0300

    Added EnumerableUtils

---
+++ Assets/Scripts/Utils/EnumerableUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class EnumerableUtils
+    {
+        public static TResult GetRandom<TResult>(this IEnumerable<TResult> self) where TResult : Object
+        {
+            var selfListed = self.ToList();
+            
+            if (selfListed.Count == 0)
+            {
+                Debug.LogWarning("Couldn't get random value from the list. It is empty. Returned null");
+                return null;
+            }
+
+            var idx = Random.Range(0, selfListed.Count);
+            var value = selfListed[idx];
+            return value;
+        }
+    }
+}
