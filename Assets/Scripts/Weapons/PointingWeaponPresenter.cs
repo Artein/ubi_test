6d175f342c19e596169e5e54cf0d32fa86792659
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-28 11:30:35 +0300

    Added WeaponController and PointingWeaponPresenter

---
+++ Assets/Scripts/Weapons/PointingWeaponPresenter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class PointingWeaponPresenter : MonoBehaviour, IWeaponPresenter
+    {
+        public Vector3 BulletSpawnPosition => transform.position;
+        public Quaternion BulletSpawnRotation => transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace Weapons
+{
+    public class WeaponController : MonoBehaviour, IWeapon
+    {
+        [Inject] private WeaponSettings _settings;
+
+        public DateTime LastShootTime { get; private set; } = DateTime.Now;
+
+        private float MillisecondsBetweenShots => 1000f * 60f / _settings.ShootsPerMinute;
+        private bool CanShoot => (DateTime.Now - LastShootTime).TotalMilliseconds <= MillisecondsBetweenShots;
+
+        void IWeapon.DoShoot()
+        {
+            if (!CanShoot)
+            {
+                return;
+            }
+            
+            LastShootTime = DateTime.Now;
+            
+            // todo: spawn bullet from prefab
+            // todo: must be poolable factory
+        }
+    }
+}
