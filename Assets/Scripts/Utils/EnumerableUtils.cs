e5f3bdc696f4e3c3dcaf5267af985422146a237b
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-28 14:02:18 +0300

    Added TryGetRandom() enumarable helper for value-types

--- Assets/Scripts/Utils/EnumerableUtils.cs
+++ Assets/Scripts/Utils/EnumerableUtils.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Utils
 {
     public static class EnumerableUtils
     {
+        [CanBeNull]
         public static TResult GetRandom<TResult>(this IEnumerable<TResult> self) where TResult : Object
+        {
+            var selfListed = self.ToList();
+            if (!TryGetRandom<TResult>(selfListed, out TResult result))
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        // for value types
+        public static bool TryGetRandom<TResult>(this IEnumerable<TResult> self, out TResult result)
         {
             var selfListed = self.ToList();
             
             if (selfListed.Count == 0)
             {
                 Debug.LogWarning("Couldn't get random value from the list. It is empty. Returned null");
-                return null;
+                result = default;
+                return false;
             }
 
             var idx = Random.Range(0, selfListed.Count);
-            var value = selfListed[idx];
-            return value;
+            result = selfListed[idx];
+            return true;
         }
     }
 }
