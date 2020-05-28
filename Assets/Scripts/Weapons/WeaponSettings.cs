322aa4fe6983c1f58a0d94a7f4c91f1ed5be7bdf
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-28 11:23:32 +0300

    Added WeaponSettings

---
+++ Assets/Scripts/Weapons/WeaponSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    [CreateAssetMenu(fileName = "WeaponSettings", menuName = "Orbitality/Weapon Settings")]
+    public class WeaponSettings : ScriptableObject
+    {
+        [SerializeField, Min(0.01f)] private float WeaponShotsPerMinute = 1;
+
+        public float ShootsPerMinute => WeaponShotsPerMinute;
+    }
+}
