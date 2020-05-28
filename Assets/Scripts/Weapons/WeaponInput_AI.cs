4d96a6f4f6ee180ce578e4d0b326f5436177fa7a
artyom.sukhliak <artyom.suhlyak@gmail.com>
2020-05-28 11:31:22 +0300

    Added weapon inputs

---
+++ Assets/Scripts/Weapons/WeaponInput_AI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using Utils;
+
+namespace Weapons
+{
+    public class WeaponInput_AI : BaseWeaponInput
+    {
+        [SerializeField, MinMax(0, 10)] private MinMax ShootDelayRange;
+        [SerializeField, MinMax(0, 10)] private MinMax InitialShootDelayRange; 
+
+        private void OnEnable()
+        {
+            StartCoroutine(MocShootingRoutine());
+        }
+
+        private IEnumerator MocShootingRoutine()
+        {
+            yield return new WaitForSeconds(InitialShootDelayRange.RandomValue);
+
+            do
+            {
+                Weapon.DoShoot();
+                
+                yield return new WaitForSeconds(ShootDelayRange.RandomValue);
+            } while (enabled);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponInput_Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class WeaponInput_Player : BaseWeaponInput
+    {
+        private void Update()
+        {
+            // todo: handle rotation and fire here
+            // todo: use new Input system
+            if (Input.GetMouseButtonDown(0)) // primary button
+            {
+                Weapon.DoShoot();
+            }
+        }
+    }
+}
